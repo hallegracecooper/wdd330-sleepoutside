3b35f1629d2267dbf1791d5885461dc4379aafa5
hallegracecooper <dri19003@byui.edu>
2025-05-13 18:03:23 -0600

    items in cart

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -2,22 +2,36 @@ function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error("Bad Response");
+    throw new Error(`Bad Response: ${res.status} ${res.statusText}`);
   }
 }
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `../json/${this.category}.json`;
+    this.path = `/json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  async getData() {
+    try {
+      const response = await fetch(this.path);
+      const data = await convertToJson(response);
+      return data;
+    } catch (err) {
+      console.error("Error loading product data:", err);
+      throw err;
+    }
   }
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    try {
+      const products = await this.getData();
+      const product = products.find((item) => item.Id === id);
+      if (!product) {
+        throw new Error(`Product with ID ${id} not found`);
+      }
+      return product;
+    } catch (err) {
+      console.error("Error finding product:", err);
+      throw err;
+    }
   }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,32 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart") || [];
+  // If cart is not an array, make it one
+  if (!Array.isArray(cart)) {
+    cart = [cart];
+  }
+  cart.push(product);
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// wait for the DOM to load before adding event listeners
+function init() {
+  const addToCartButton = document.getElementById("addToCart");
+  if (addToCartButton) {
+    addToCartButton.addEventListener("click", addToCartHandler);
+  } else {
+    console.error("Add to cart button not found in the DOM");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", init);
