9758f2ea93f18b75a297a08fb1fb41da26ab8680
hallegracecooper <dri19003@byui.edu>
2025-05-13 20:18:04 -0600

    team2

--- src/js/product.js
+++ src/js/product.js
@@ -1,32 +1,9 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
 
-const dataSource = new ProductData("tents");
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
 
-function addProductToCart(product) {
-  let cart = getLocalStorage("so-cart") || [];
-  // If cart is not an array, make it one
-  if (!Array.isArray(cart)) {
-    cart = [cart];
-  }
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// wait for the DOM to load before adding event listeners
-function init() {
-  const addToCartButton = document.getElementById("addToCart");
-  if (addToCartButton) {
-    addToCartButton.addEventListener("click", addToCartHandler);
-  } else {
-    console.error("Add to cart button not found in the DOM");
-  }
-}
-
-document.addEventListener("DOMContentLoaded", init);
+const product = new ProductDetails(productId, dataSource);
+product.init();
