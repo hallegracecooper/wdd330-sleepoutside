0ba49666288c517edd6826746bfdb791905b0dfa
hallegracecooper <dri19003@byui.edu>
2025-06-01 16:17:13 -0600

    individual3

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import ProductData from './ProductData.js';
 import ProductList from './ProductList.mjs';
 import { loadHeaderFooter } from "./utils.mjs";
+import Alert from './Alert.js';
 
 const dataSource = new ProductData('tents');
 const element = document.querySelector('.product-list');
 const productList = new ProductList('tents', dataSource, element);
+
+const alert = new Alert();
+alert.init();
 productList.init();
 
 loadHeaderFooter();
